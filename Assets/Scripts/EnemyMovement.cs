71ecdc5cca9e586f30250929fb49fe901cb4cbfb
carsten <cm4@live.de>
2023-10-06 23:36:20 +0200

    add enemy prefab and enemies to default scene

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private Rigidbody2D enemyRigid;
+    public Rigidbody2D playerRigid;
+    public float speedMod = 1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var xDif = playerRigid.position.x - enemyRigid.position.x;
+        var yDif =playerRigid.position.y - enemyRigid.position.y;
+
+        Vector2 velocity = new Vector2(xDif, yDif).normalized;
+
+        enemyRigid.velocity = velocity * speedMod;
+    }
+}
