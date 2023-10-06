086a03b408902b300ec95e7b4ff6ec0e5f9e0a7a
Michael von Bargen <michael.vonbargen@holisticon.de>
2023-10-07 00:38:59 +0200

    add abstract enemy, refactor player hit

---
+++ Assets/Enemy/Enemy.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+namespace Enemy
+{
+    public abstract class Enemy : MonoBehaviour
+    {
+        protected int Health;
+        protected float MovementSpeed;
+        protected float AttackDistance;
+        protected int Damage;
+
+        protected virtual void Attack()
+        {
+            var player = new Rigidbody2D();
+            
+            
+        }
+
+        private void DoDamage(int damage)
+        {
+            
+        }
+        
+
+        protected void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.name == "player")
+            {
+               other.gameObject.GetComponent<PlayerHealth>().ProcessHit(this.Damage);
+            } 
+        }
+    }
+}
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -7,16 +7,12 @@ public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 4;
 
-    public delegate void HitEvent(int damage);
-    public static event HitEvent OnHit;
-
     public delegate void DeathEvent();
     public static event DeathEvent OnDeath;
 
     // Start is called before the first frame update
     void Start()
     {
-        OnHit += ProcessHit;
     }
 
     // Update is called once per frame
@@ -25,7 +21,7 @@ public class PlayerHealth : MonoBehaviour
         
     }
 
-    private void ProcessHit(int damage)
+    public void ProcessHit(int damage)
     {
         maxHealth -= damage;
 
