35e0955b22da434451bc25cdb221ab6fd47e468d
Michael von Bargen <michael.vonbargen@holisticon.de>
2023-10-07 01:20:17 +0200

    add enemy damage

--- Assets/Enemy/Enemy.cs
+++ Assets/Enemy/Enemy.cs
@@ -1,34 +1,39 @@
-using System;
 using UnityEngine;
 
 namespace Enemy
 {
     public abstract class Enemy : MonoBehaviour
     {
-        protected int Health;
-        protected float MovementSpeed;
+        public int health = 1;
+        public int damage = 1;
+        private readonly string playerName = "Player";
         protected float AttackDistance;
-        protected int Damage;
+        protected float MovementSpeed;
+
+        protected void OnCollisionEnter2D(Collision2D other)
+        {
+            ProcessHit(other);
+        }
+
+        private void OnCollisionStay2D(Collision2D other)
+        {
+            ProcessHit(other);
+        }
+
+        private void ProcessHit(Collision2D other)
+        {
+            if (other.gameObject.name != playerName) return;
+            Debug.Log($"Player was hit with {damage} Damage");
+            other.gameObject.GetComponent<PlayerHealth>().ProcessHit(damage);
+        }
 
         protected virtual void Attack()
         {
             var player = new Rigidbody2D();
-            
-            
         }
 
         private void DoDamage(int damage)
         {
-            
-        }
-        
-
-        protected void OnCollisionEnter(Collision other)
-        {
-            if (other.gameObject.name == "player")
-            {
-               other.gameObject.GetComponent<PlayerHealth>().ProcessHit(this.Damage);
-            } 
         }
     }
 }
