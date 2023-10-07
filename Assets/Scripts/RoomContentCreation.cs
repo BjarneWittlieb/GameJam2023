4082c3c93681eb600da5411d53fc1da95451e61d
Michael von Bargen <michael.vonbargen@holisticon.de>
2023-10-07 22:37:06 +0200

    room difficulty can be configured

--- Assets/Scripts/RoomContentCreation.cs
+++ Assets/Scripts/RoomContentCreation.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class RoomContentCreation : MonoBehaviour
 {
     public NavMeshSurface navMesh;
+    public int difficulty = 1;
 
     // Start is called before the first frame update
     private void Start()
     {
-        var room = RoomFactory.CreateRoom(transform, new RoomSettings(5));
+        var room = RoomFactory.CreateRoom(transform, new RoomSettings(difficulty));
 
         room.PopulateObstacles();
 
