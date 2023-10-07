1fcc17aced1baa6b88899937c1ecb75e751d5ca6
Armbrust, Cornelius <Cornelius.Armbrust@adesso.de>
2023-10-07 12:57:06 +0200

    RangedAttack.cs und so

--- Assets/Scripts/PlayerScripts/RangedAttack.cs
+++ Assets/Scripts/PlayerScripts/RangedAttack.cs
@@ -1,10 +1,14 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace PlayerScripts
 {
     public class RangedAttack : MonoBehaviour
     {
         [SerializeField] private GameObject projectile;
+        
+        [SerializeField] private Transform shotPoint;
+        [SerializeField] private Transform gun;
         
         private void Update()
         {
@@ -14,8 +18,25 @@ namespace PlayerScripts
 
         private void FireProjectile()
         {
-            var transform1 = transform;
-            Instantiate(projectile, transform1.position, transform1.rotation);
+            var differance = Input.mousePosition - gun.transform.position;
+            var rotZ       = Mathf.Atan2(differance.y, differance.x) * Mathf.Rad2Deg;
+            gun.transform.rotation = Quaternion.Euler(0f, 0f, rotZ);
+            
+            Vector3 mouseScreenPosition = Input.mousePosition;
+            Vector3 mouseWorldPosition  = Camera.main.ScreenToWorldPoint(mouseScreenPosition);
+
+            Vector2 directionToMouse = (Vector2)(mouseWorldPosition - transform.position);
+            float   angle            = Mathf.Atan2(directionToMouse.y, directionToMouse.x) * Mathf.Rad2Deg;
+
+            gun.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+            
+            Instantiate(projectile, shotPoint.position, shotPoint.transform.rotation);
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(Input.mousePosition, 1f);
         }
     }
 }
