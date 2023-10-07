51ce56c2eec3d2533a9ff556696a63ec8c2a8da2
Michael von Bargen <michael.vonbargen@holisticon.de>
2023-10-07 17:02:13 +0200

    WIP WIP

---
+++ Assets/GameOverHandler.cs
@@ -0,0 +1,19 @@
+using PlayerScripts;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        PlayerHealth.OnDeath += ShowGameOver;
+    }
+
+
+    private void ShowGameOver()
+    {
+        gameObject.SetActive(true);
+        SceneManager.LoadScene("Game-Over-Scene", LoadSceneMode.Single);
+    }
+}
