ffa2bfa1ec49572f4f223345a8d2d8f04bbf216c
Michael von Bargen <michael.vonbargen@holisticon.de>
2023-10-08 03:32:48 +0200

    dynamically create obstacles

--- Assets/Scripts/RoomGeneration/RoomCreator.cs
+++ Assets/Scripts/RoomGeneration/RoomCreator.cs
@@ -1,26 +1,23 @@
 using NavMeshPlus.Components;
 using RoomGeneration;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class RoomCreator : MonoBehaviour
 {
     public NavMeshSurface navMesh;
+    public float startingDifficulty = 1f;
+    private float currentDifficulty;
 
     private GameObject player;
     private Vector3 playerStartPos;
 
     private Room room;
-    public float startingDifficulty = 1f;
-    private float currentDifficulty;
 
     public LevelDoor levelDoor1;
     public LevelDoor levelDoor2;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         player = GameObject.Find("Player");
         playerStartPos = player.transform.position;
@@ -44,14 +41,15 @@ public class RoomCreator : MonoBehaviour
 
     public void DestroyAndBuildRoom(Room newRoom)
     {
-        if (room != null)
-        {
-            room.DestroyRoom();
-        }
-        room = newRoom;
+        if (room != null) room.DestroyRoom();
+        room = RoomFactory.CreateRoom(transform, new RoomSettings((int)currentDifficulty));
 
         room.PopulateObstacles();
+        Physics2D.SyncTransforms();
         navMesh.BuildNavMesh();
+        navMesh.BuildNavMeshAsync();
+
+        //collectSources2d.room;
         room.PopulateEnemies();
 
         player.transform.position = playerStartPos;
--- Assets/Scripts/RoomGeneration/RoomFactory.cs
+++ Assets/Scripts/RoomGeneration/RoomFactory.cs
@@ -1,39 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using UnityEngine;
 
 namespace RoomGeneration
 {
     public static class RoomFactory
     {
-        public static Room CreateRoom(Transform levelRoot, RoomSettings settings) {
+        public static Room CreateRoom(Transform levelRoot, RoomSettings settings)
+        {
             var room = new Room(levelRoot, GetRoomReward());
 
             PopulateRoom(room, settings.TotalEnemyScore, settings.MaximalEnemyBatchScore, RoomObject.Enemies);
-            PopulateRoom(room, settings.TotalObstacleScore, settings.MaximalObstacleBatchScore, new RoomObject[] { });
+            PopulateRoom(room, settings.TotalObstacleScore, settings.MaximalObstacleBatchScore, RoomObject.Obstacles);
 
             return room;
         }
 
         private static RoomReward GetRoomReward()
         {
-            return RoomReward.Rewards.ElementAt(UnityEngine.Random.Range(0, RoomReward.Rewards.Length));
+            return RoomReward.Rewards.ElementAt(Random.Range(0, RoomReward.Rewards.Length));
         }
 
         private static void PopulateRoom(Room room, int maximumScore, int maximumBatchScore, RoomObject[] ojbects)
         {
-            int scoreLeft = maximumScore;
+            var scoreLeft = maximumScore;
             while (true)
             {
-                var availableObjects = ojbects.Where(e => e.DifficultyScore < scoreLeft && e.DifficultyScore < maximumBatchScore);
-                if (!availableObjects.Any())
-                {
-                    break;
-                }
-                var roomObject = availableObjects.ElementAt(UnityEngine.Random.Range(0, availableObjects.Count()));
+                var availableObjects = ojbects.Where(e =>
+                    e.DifficultyScore < scoreLeft && e.DifficultyScore < maximumBatchScore);
+                if (!availableObjects.Any()) break;
+                var roomObject = availableObjects.ElementAt(Random.Range(0, availableObjects.Count()));
                 room.AddRoomObject(roomObject);
                 scoreLeft -= roomObject.DifficultyScore;
             }
